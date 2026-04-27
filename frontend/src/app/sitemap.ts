b8b45eb1060703ea117bcb5ed7de61c4ddced17f
bohdeveloper <ohb.seven@gmail.com>
2026-04-27 16:29:43 +0200

    Modificar sitemap.ts

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -7,7 +7,7 @@ import { MetadataRoute } from "next";
 export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.bohdeveloper.com";
+  const baseUrl = "https://bohdeveloper.com";
 
   return [
     {
