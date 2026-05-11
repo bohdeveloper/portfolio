62a87b87967dec76b62787de3a77d4936399f018
Borja Olazabal <ohb.seven@gmail.com>
2026-05-11 13:36:55 +0200

    Cambio en proyectos, añadir landings

--- frontend/src/components/sections/Proyectos.tsx
+++ frontend/src/components/sections/Proyectos.tsx
@@ -1,27 +1,13 @@
 export default function Proyectos() {
   const proyectos = [
     {
-      nombre: "Nitflex",
-      slug: "nitflex",
-      descripcion:
-        "Aplicación web de streaming educativo inspirada en Netflix, desarrollada como proyecto de aprendizaje full stack.",
-      arquitectura: "Web App didáctica NoSQL",
-      tecnologias: [
-        "React",
-        "TypeScript",
-        "Tailwind",
-        "Express",
-        "Mongoose",
-        "MongoDB",
-        "TMDB API",
-      ],
-      github: "https://github.com/bohdeveloper/nitflex",
-    },
-    {
       nombre: "Unyona",
       slug: "unyona",
-      descripcion:
+      descripcion1:
         "Plataforma social orientada a eventos y gestión de comunidades.",
+      descripcion2:
+        "Se ha desarrollado una landing como demostración de la arquitectura y funcionalidades del proyecto, análisis del producto y captación de leads.",
+      landing: "https://unyona.com",
       arquitectura: "Web App SQL",
       tecnologias: [
         "React",
@@ -37,11 +23,31 @@ export default function Proyectos() {
     {
       nombre: "Diamadmin",
       slug: "diamadmin",
-      descripcion:
+      descripcion1:
         "Aplicación de gestión administrativa modular y altamente configurable.",
+      descripcion2:
+        "Se ha desarrollado una landing como demostración de la arquitectura y funcionalidades del proyecto, análisis del producto y captación de leads.",
+      landing: "https://diamadmin.com",
       arquitectura: "Arquitectura modular",
       tecnologias: ["Angular", "Spring Boot", "PostgreSQL"],
       github: "https://github.com/bohdeveloper/diamadmin",
+    },
+    {
+      nombre: "Nitflex",
+      slug: "nitflex",
+      descripcion:
+        "Aplicación web de streaming educativo inspirada en Netflix, desarrollada como proyecto de aprendizaje full stack.",
+      arquitectura: "Web App didáctica NoSQL",
+      tecnologias: [
+        "React",
+        "TypeScript",
+        "Tailwind",
+        "Express",
+        "Mongoose",
+        "MongoDB",
+        "TMDB API",
+      ],
+      github: "https://github.com/bohdeveloper/nitflex",
     },
     {
       nombre: "DevHelper",
@@ -83,48 +89,75 @@ export default function Proyectos() {
 
       <div className="grid gap-10 md:grid-cols-2">
         {proyectos.map((p, i) => (
-          <article
-            key={i}
-            className="relative flex flex-col h-full border border-gray-300 dark:border-gray-700 rounded-lg p-6 bg-white dark:bg-[#0d0d0d] hover:border-cyan-400 hover:scale-[1.02] transition-all"
-          >
-            <div className="flex-1">
-              <span className="absolute top-4 right-4 text-xs px-3 py-1 rounded-full border border-cyan-400 text-primary bg-cyan-400/10 dark:bg-cyan-400/20">
-                En desarrollo
-              </span>
-
-              <h2 className="text-xl font-semibold text-black dark:text-white">
-                {p.nombre}
-              </h2>
-
-              <p className="text-gray-600 dark:text-gray-400 mt-2">
-                {p.descripcion}
-              </p>
-
-              <p className="text-sm text-primary mt-3 font-medium">
-                {p.arquitectura}
-              </p>
-
-              <div className="mt-4">
-                <h3 className="text-sm font-semibold text-black dark:text-white mb-2">
-                  Tecnologías utilizadas
-                </h3>
-                <ul className="list-disc pl-5 space-y-1 text-gray-700 dark:text-gray-300">
-                  {p.tecnologias.map((t, idx) => (
-                    <li key={idx}>{t}</li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-
-            <a
-              href={p.github}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="mt-6 inline-block text-center px-4 py-2 border border-cyan-400 text-primary rounded hover:bg-cyan-400 hover:text-black transition font-medium"
+          <a href={p.landing} target="_blank" rel="noopener noreferrer" >  
+            <article
+              key={i}              
+              className={`
+                relative flex flex-col h-full rounded-lg p-6
+                bg-white dark:bg-[#0d0d0d]
+                border-2
+                ${
+                  p.landing
+                    ? "border-primary hover:scale-[1.02] hover:shadow-[0_0_20px_rgba(0,231,235,0.25)] cursor-pointer transition-all"
+                    : "border-gray-300 dark:border-gray-700"
+                }
+              `}
             >
-              Ver código en GitHub →
-            </a>
-          </article>
+              <div className="flex-1">
+                <span className="absolute top-4 right-4 text-xs px-3 py-1 rounded-full border border-cyan-400 text-primary bg-cyan-400/10 dark:bg-cyan-400/20">
+                  En desarrollo
+                </span>
+
+                <h2 className="text-2xl font-semibold text-black dark:text-white">
+                  {p.nombre}
+                </h2>
+
+                <p className="text-lg text-gray-600 dark:text-gray-400 mt-2">
+                  {p.descripcion}
+                </p>
+
+                <p className="text-lg text-gray-600 dark:text-gray-400 mt-2">
+                  {p.descripcion1}
+                </p>
+
+                <p className="text-sm text-primary mt-2 pr-14">
+                  {p.descripcion2}
+                </p>
+
+                { p.landing ?
+                  <div className="my-6 border-t border-primary" /> : <div className="my-6 border-t border-gray-200 dark:border-gray-700" />
+                }
+
+                <p className="text-lg text-primary mt-3 font-medium">
+                  {p.arquitectura}
+                </p>
+                
+                { p.landing ?
+                  <div className="my-6 border-t border-primary" /> : <div className="my-6 border-t border-gray-200 dark:border-gray-700" />
+                }
+
+                <div className="mt-4">
+                  <h3 className="text-sm font-semibold text-black dark:text-white mb-2">
+                    Tecnologías utilizadas
+                  </h3>
+                  <ul className="list-disc pl-5 space-y-1 text-gray-700 dark:text-gray-300">
+                    {p.tecnologias.map((t, idx) => (
+                      <li key={idx}>{t}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+
+              <a
+                href={p.github}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-6 inline-block text-center px-4 py-2 border border-cyan-400 text-primary rounded hover:bg-cyan-400 hover:text-black transition font-medium"
+              >
+                Ver código en GitHub →
+              </a>
+            </article>
+          </a>
         ))}
       </div>
     </section>
